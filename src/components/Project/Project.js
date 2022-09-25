6c97ad078af564bdd319f3a4728654e0270ff547
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-09-25 14:09:10 +0200

    update styling

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,7 +4,7 @@ export const Project = ({ name, lesson}) => {
   return (
     <div class="project-wrapper">
       <h3>{name}</h3>
-      <span>{lesson}</span>
+      <span id="lesson"><i>What I've learned:</i> <br /><br />{lesson}</span>
     </div>
   )
 }
