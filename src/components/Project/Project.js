1faa277dd1d6c5113a8dcd2b9a7d200d40f63926
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-09-28 16:16:30 +0200

    update db and change diction in project component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,7 +4,7 @@ export const Project = ({ id, name, lesson, onDragStart}) => {
   return (
     <div className="project-wrapper" draggable onDragStart={(e) => onDragStart(e, id)}>
       <h3>{name}</h3>
-      <span id="lesson"><i>What I've learned:</i> <br /><br />{lesson}</span>
+      <span id="lesson"><i>Technical summary:</i> <br /><br />{lesson}</span>
     </div>
   )
 }
